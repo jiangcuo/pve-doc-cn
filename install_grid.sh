0865f209b431345ab813dfd4b62ad3d925696f6d
jiangcuo <jiangcuo@bingsin.com>
2023-12-06 00:19:46 +0800

    Update install_grid.sh

--- install_grid.sh
+++ install_grid.sh
@@ -1,47 +1,23 @@
 #!/bin/bash
 #https://pve-doc-cn.readthedocs.io/zh_CN/pve-nvidia-vgpu/
-#bash install_grid.sh unlock to install vgpu_unlock
+#bash `install_grid.sh unlock` to install vgpu_unlock
+nvidia_version=""
 vgpu_unlock_rs_ver="v2.3.1"
-vgpu_unlock_rs_url="https://mirrors.apqa.cn/d/vGPU/vgpu_unlock/rust/libvgpu_unlock_rs_$vgpu_unlock_rs_ver.so"
-vgpu_unlock_rs_conf="https://mirrors.apqa.cn/d/vGPU/vgpu_unlock/vgpu_unlock.conf"
+vgpu_unlock_rs_url="https://foxi.buduanwang.vip/pan/vGPU/vgpu_unlock/rust/libvgpu_unlock_rs_$vgpu_unlock_rs_ver.so"
+vgpu_unlock_rs_conf="https://foxi.buduanwang.vip/pan/vGPU/vgpu_unlock/vgpu_unlock.conf"
 main_kernelver=$(uname -r|cut -d "." -f1)
 minor=$(uname -r|cut -d "." -f2)
-if [ "$main_kernelver" -eq 5 ];then
-    if [ "$minor" -ge 16 ];then
-        echo "$main_kernelver.$minor is not support"
-        exit 0
-    elif [ "$minor" -ge 3 ];then
-        nvidia_version="510.47.03"
-    else
-        echo "$main_kernelver.$minor is not support"
-        exit 0
-    fi
-elif [ "$main_kernelver" -eq 4 ];then
-    if [ "$minor" -lt 16 ];then
-        nvidia_version="470.103.02"
-    else
-        echo "$main_kernelver.$minor is not support"
-        exit 0
-    fi
-else
-echo "$main_kernelver.$minor is not support"
-exit 0
-fi
-echo  your kernel $main_kernelver.$minor match $nvidia_version
-
-nvidia_pkg="NVIDIA-Linux-x86_64-$nvidia_version-vgpu-kvm-custom.run"
-nvidia_url="https://mirrors.apqa.cn/d/vGPU/vgpu_unlock/drivers/$nvidia_pkg"
 
 grub_check(){
     if [ -e /etc/kernel/proxmox-boot-uuids ]
     then
-    echo "引导为Systemd-boot"
-    echo "正在修改cmdline"
-    edit_cmdline
+        echo "引导为Systemd-boot"
+        echo "正在修改cmdline"
+        edit_cmdline
     else
-    echo "引导为grub"
-    echo "正在修改grub"    
-    edit_grub
+        echo "引导为grub"
+        echo "正在修改grub"    
+        edit_grub
     fi
 }
 
@@ -61,7 +37,7 @@ modiy_modules(){
 
 edit_cmdline(){
     cp /etc/kernel/cmdline /opt/foxi_backup/cmdline_$(date +%s)
-    echo 'GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on iommu=pt"' > /etc/kernel/cmdline
+    echo 'root=ZFS=rpool/ROOT/pve-1 boot=zfs intel_iommu=on iommu=pt' > /etc/kernel/cmdline
     proxmox-boot-tool refresh > /dev/null 2>&1
     echo "cmdline修改完成"
 }
@@ -74,25 +50,18 @@ edit_grub(){
     echo "grub修改完成"
 }
 
-pkg_install(){
-    apt update 
-    apt install dkms build-essential jq pve-headers-`uname -r` -y || apt install -f dkms build-essential jq pve-headers-`uname -r` -y
-    wget -P /tmp/ http://ftp.br.debian.org/debian/pool/main/m/mdevctl/mdevctl_0.81-1_all.deb
-    dpkg -i /tmp/mdevctl_0.81-1_all.deb
-}
-
-
 install_grid(){
-    curl -L -O $nvidia_url 
-    sh $nvidia_pkg --dkms -q -s
+    curl -L -O $nvidia_url  || error_log "nvidia driver download failed!"
+    sh $nvidia_pkg --dkms -q -s -z --no-x-check || error_log "nvidia driver install failed!"
     rm $nvidia_pkg
 }
+
 vgpu_unlock(){
-    mkdir /etc/systemd/system/nvidia-vgpud.service.d/
-    mkdir /etc/systemd/system/nvidia-vgpu-mgr.service.d/
-    mkdir /usr/share/vgpu_unlock_rs/
-    wget -P /tmp $vgpu_unlock_rs_url
-    wget -P /tmp $vgpu_unlock_rs_conf
+    mkdir /etc/systemd/system/nvidia-vgpud.service.d/ -p
+    mkdir /etc/systemd/system/nvidia-vgpu-mgr.service.d/ -p 
+    mkdir /usr/share/vgpu_unlock_rs/ -p
+    wget -P /tmp $vgpu_unlock_rs_url ||  error_log "vgpu unlock rust file download failed!"
+    wget -P /tmp $vgpu_unlock_rs_conf || error_log "vgpu unlock rust config download failed!"
     cp /tmp/libvgpu_unlock_rs_$vgpu_unlock_rs_ver.so /usr/share/vgpu_unlock_rs/libvgpu_unlock_rs.so
     cp /tmp/vgpu_unlock.conf /etc/systemd/system/nvidia-vgpud.service.d/
     cp /tmp/vgpu_unlock.conf /etc/systemd/system/nvidia-vgpu-mgr.service.d/
@@ -103,6 +72,52 @@ vgpu_unlock(){
     systemctl start nvidia-vgpu-mgr.service
 }
 
+error_log(){
+    echo $1
+    exit 1;
+}
+
+
+install_pkg(){
+    apt update && apt install -y build-* mdevctl dkms pve-headers-`uname -r` || error_log "install pkg failed"
+}
+
+
+if [ ! -n "$nvidia_version" ];then
+    if [ "$main_kernelver" -eq 5 ];then
+        if [ "$minor" -ge 16 ];then
+            echo "$main_kernelver.$minor is not support"
+            exit 0
+        elif [ "$minor" -ge 3 ];then
+            nvidia_version="510.47.03"
+        else
+            echo "$main_kernelver.$minor is not support"
+            exit 0
+        fi
+    elif [ "$main_kernelver" -eq 4 ];then
+        if [ "$minor" -lt 16 ];then
+            nvidia_version="470.103.02"
+        else
+            echo "$main_kernelver.$minor is not support"
+            exit 0
+        fi
+    elif [ "$main_kernelver" -eq 6 ];then
+        if [ "$minor" -ge 1 ];then
+            nvidia_version="535.129.03"
+        else
+            echo "$main_kernelver.$minor is not support"
+            exit 0
+        fi
+    else
+        echo "$main_kernelver.$minor is not support"
+        exit 0
+    fi
+fi
+
+echo  your kernel $main_kernelver.$minor match $nvidia_version
+
+nvidia_pkg="NVIDIA-Linux-x86_64-$nvidia_version-vgpu-kvm-custom.run"
+nvidia_url="https://foxi.buduanwang.vip/pan/vGPU/vgpu_unlock/drivers/$nvidia_pkg"
 
 echo "这是一个自动配置Nvidia-vGPU的脚本"
 echo "本脚本不检测硬件类型，请自己确保符合条件"
@@ -110,6 +125,7 @@ echo "本脚本不检测硬件类型，请自己确保符合条件"
 read -p "请按y/Y继续" configure
 if [ "$configure" = "y" ] || [ "$configure" = "Y" ] 
 then
+install_pkg
 echo "开始检测引导类型"
 mkdir /opt/foxi_backup > /dev/null 2>&1
 grub_check
@@ -118,16 +134,8 @@ echo "输入错误，脚本退出"
 exit 0
 fi
 modiy_modules
-pkg_install
-
-if  [ "$1" = "unlock" ];then
 install_grid
 vgpu_unlock
-echo "vgpu_unlock done"
-else
-install_grid
-fi
-
 
 echo "脚本执行完成，请重启"
 echo "其中grub和modules文件已经备份到/opt/foxi_backup目录下"
